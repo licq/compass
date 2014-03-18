ea743275df1696f3e24941649579e72b138ead03
Li Chaoqun <lee.chaoqun@gmail.com>
2014-03-18 14:40:50 +0800

    add index page

---
+++ test/server/controllers/indexSpec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var app = require('../../../server'),
+    request = require('supertest');
+
+
+describe('/', function () {
+
+    describe('GET /anything', function () {
+        it('should return 200 with html result', function (done) {
+            request(app)
+                .get('/anything')
+                .expect(200)
+                .expect('Content-Type', /html/)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    res.body.should.match(/<html>/);
+                    done();
+                });
+        });
+    });
+
+    describe('GET /api/nothingshouldcalledthisname', function () {
+        it('should return 404 when call /api/nothing', function (done) {
+            request(app)
+                .get('/api/nothingshouldcalledthisname')
+                .expect(404)
+                .end(done);
+        });
+    });
+});
+
+
