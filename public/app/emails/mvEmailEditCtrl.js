946f5a0de1e723a437f615d3d2fd270aab90b090
Li Chaoqun <lee.chaoqun@gmail.com>
2014-03-27 14:56:44 +0800

    email edit verify

--- public/app/emails/mvEmailEditCtrl.js
+++ public/app/emails/mvEmailEditCtrl.js
@@ -5,10 +5,12 @@ angular.module('compass')
         $scope.email = mvEmail.get({_id: $routeParams.id});
 
         $scope.update = function () {
+            $scope.saving = true;
             $scope.email.$update(function () {
                 $location.path('/emails');
             }, function (err) {
                 console.log(err);
+                $scope.saving = false;
                 $scope.err = err.data;
             });
         };
