58bf770e1fc4ae1e8ba51c2e4b4fe03c7ba46164
Li Chaoqun <lee.chaoqun@gmail.com>
2014-03-31 16:05:17 +0800

    improved resume parser

--- server/parsers/resumeParser.js
+++ server/parsers/resumeParser.js
@@ -5,10 +5,8 @@ exports.parse = function (html) {
     var resume = {};
 
     var $ = cheerio.load(html);
-    var topTableTrs = $('body > table').eq(1).find('table').first().children();
-    var contactTr = topTableTrs.first().find('table').children().eq(1);
-    resume.name = contactTr.children().first().find('span').text();
-    var contactInfos = contactTr.children().eq(1).text().split('\n');
+    resume.name = $('td tr:nth-child(1) tr:nth-child(2) span').text();
+    var contactInfos = $('td tr:nth-child(1) table:nth-child(1) td:nth-child(2)').text().split('\n');
     var firstLine = contactInfos[0].split('|');
     resume.gender = firstLine[0].trim() === '男' ? 'male' : 'female';
     resume.married = firstLine[1].trim() !== '未婚';
@@ -27,11 +25,31 @@ exports.parse = function (html) {
     resume.mobile = secondLine[2].substr(6, 11);
     resume.email = contactInfos[2].split(': ')[1];
 
+    resume.selfDescription = $('.resume_p:nth-child(1)').text().trim();
+    resume.desiredPositions = $('td td td td tr:nth-child(2) td:nth-child(2)').text().split('，');
+    resume.desiredSalary = $('td td td td tr:nth-child(3) td:nth-child(2)').text();
+    resume.currentStatus = $('tr:nth-child(4) td:nth-child(2)').text();
 
-    var secondTable = $(topTableTrs).eq(1).find('td table tr td table:nth-child(4)');
-    resume.selfDescription = secondTable.find('div.resume_p').text().trim();
-    resume.desiredPositions = $(secondTable).find('table tr:nth-child(2)').text();
-    console.log(secondTable);
+    resume.workExperiences = [];
+    $('td td tr:nth-child(2) table:nth-child(4) tr').slice(0, 1).each(function (i, elem) {
+        var work = {};
+        work.dateRange = $(elem).children().first().text().trim();
+        work.work = $(elem).children().last().text().trim();
+        resume.workExperiences.push(work);
+    });
 
+    resume.practices =
+        $('tr:nth-child(6) table:nth-child(4) tr').map(function (index, elem) {
+            return {dateRange: $(elem).children().first().text().trim(),
+                content: $(elem).children().last().text().trim()};
+        });
+
+    resume.studyStatus = $('tr:nth-child(5) table:nth-child(4) td').text().trim();
+    resume.educationHistory = $('tr:nth-child(4) table:nth-child(4) td').text();
+    resume.projects = $('tr:nth-child(3) table:nth-child(4) td').html();
+
+    resume.professionalSkills = $('tr:nth-child(9) table:nth-child(4) td').text();
+    resume.papers = $('td table:nth-child(3) td').text();
+    resume.intrests = $('table:nth-child(7) td').text();
     return resume;
 };
