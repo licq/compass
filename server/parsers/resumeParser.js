dd427092c5e5653f5874da8f612e19d7108845f4
Li Chaoqun <lee.chaoqun@gmail.com>
2014-03-31 22:08:19 +0800

    add 51job parser

--- server/parsers/resumeParser.js
+++ server/parsers/resumeParser.js
@@ -53,3 +53,143 @@ exports.parse = function (html) {
     resume.intrests = $('table:nth-child(7) td').text();
     return resume;
 };
+
+
+exports.parse51Job = function (html) {
+    var resume = {};
+    var $ = cheerio.load(html);
+    resume.applyPosition = $('td tr:nth-child(1) .blue1:nth-child(2)').text().trim();
+    resume.applyDate = new Date($('tr:nth-child(3) .blue1').text());
+    resume.matchRate = $('font b').text();
+    resume.name = $('strong').text();
+    var currents = $('.blue1 b').text().split('|');
+    resume.currentStatus = currents[0];
+    resume.gender = parseGender(currents[1]);
+    resume.birthday = parseBirthday(currents[2].split('(')[1]);
+
+    resume.ghetto = $('tr:nth-child(2) tr:nth-child(2) td:nth-child(2)').text();
+    resume.mobile = $('tr:nth-child(2) tr:nth-child(3) td:nth-child(2)').text().match(/\d+/)[0];
+    resume.email = $('a.blue1').text().trim();
+    resume.job51Id = onlyNumber($('td:nth-child(2) span').text());
+
+    resume.selfDescription = $('#Cur_Val').text();
+    resume.entryTime = $('.table_set tr:nth-child(1) .text_left .text').text();
+    resume.desiredPositions = $('tr:nth-child(3) .text_left .text').text().split('，');
+    resume.desiredCities = splitByCommas($('.table_set tr:nth-child(4) .text_left .text').text());
+    resume.desiredSalary = $('tr:nth-child(5) .text_left .text').text();
+    resume.desiredPosition = $('tr:nth-child(6) .text_left .text').text();
+
+    resume.workExperiences = $('table:nth-child(3) tr:nth-child(4) table').map(function (i, table) {
+        var items = replaceNewLineAndSpace($(table).children().first().text()).split(/--|: |（|）/);
+        return {
+            from: items[0],
+            to: items[1],
+            company: items[2],
+            industry: $('tr:nth-child(2) td:nth-child(2)', $(table)).text(),
+            department: $('tr:nth-child(3) td:nth-child(1)', $(table)).text().trim(),
+            job: $('tr:nth-child(3) td:nth-child(2)', $(table)).text().trim(),
+            jobResponsibility: $('tr:nth-child(4)', $(table)).text().trim(),
+        };
+    });
+
+    resume.projects = [];
+
+    var $trs = $('tr:nth-child(9) td table tr');
+    var projectsLength = ($trs.length + 1) / 7;
+    for (var pin = 0; pin < projectsLength; pin++) {
+        var project = {};
+        project.name = replaceNewLineAndSpace($trs.eq(pin * 7 + 0).text());
+        project.softwareEnviroment = replaceNewLineAndSpace($('td:nth-child(2)', $trs.eq(1 + pin * 7)).text());
+        project.hardwareEnviroment = replaceNewLineAndSpace($('td:nth-child(2)', $trs.eq(2 + pin * 7)).text());
+        project.developmentTools = replaceNewLineAndSpace($('td:nth-child(2)', $trs.eq(3 + pin * 7)).text());
+        project.description = replaceNewLineAndSpace($('td:nth-child(2)', $trs.eq(4 + pin * 7)).text());
+        project.responsibility = replaceNewLineAndSpace($('td:nth-child(2)', $trs.eq(5 + pin * 7)).text());
+        resume.projects.push(project);
+    }
+
+    resume.educationHistory = _.map(parseTable($('tr:nth-child(14) table'), $), function (line) {
+        return { dateRange: line[0],
+            university: line[1],
+            major: line[2],
+            educationLevel: line[3]
+        }
+    });
+
+    var trainingTable = parseTable($('tr:nth-child(20) table'), $);
+    resume.trainingHistory = [];
+    resume.trainingHistory.push({
+        dateRange: trainingTable[0][0],
+        organization: trainingTable[0][1],
+        subject: trainingTable[0][2],
+        status: trainingTable[0][3],
+        description: trainingTable[1][0]
+    });
+
+    resume.certificates = _.map(parseTable($('tr:nth-child(25) table'),$), function(line){
+        return {
+            date: line[0],
+            subject: line[1],
+            score: parseInt(line[2],10)
+        };
+    });
+
+    resume.languageSkills = _.map(parseTable($('tr:nth-child(30) table table'), $),function(line){
+        return {
+            subject: line[0],
+            skill: line[1]
+        };
+    });
+
+    resume.professionalSkills = _.map(parseTable($('tr:nth-child(35) table'),$).slice(2),function(line){
+        return {
+            subject: line[0],
+            level: line[1],
+            howlong: line[2]
+        };
+    });
+
+    return resume;
+};
+
+function parseGender(input) {
+    return input.trim() === '男' ? 'male' : 'female';
+}
+
+function parseBirthday(input) {
+    var match = input.match(/\d+/g);
+    return {
+        birthYear: match[0],
+        birthMonth: match[1],
+        birthDate: match[2]
+    };
+}
+
+function onlyNumber(input) {
+    var match = input.match(/\d+/g);
+    return match[0];
+}
+
+function splitByCommas(input) {
+    return input.split(/,|,|，/);
+}
+
+function replaceNewLineAndSpace(input) {
+    if (Array.isArray(input)) {
+        return _.forEach(input, function (item) {
+            return item.replace(/\n| +/g, '');
+        });
+    }
+    return input.replace(/\n| +/g, '');
+}
+
+function parseTable(table, $) {
+    var items = [];
+    $('tr', $(table)).each(function () {
+        var item = [];
+        $(this).find('td').each(function () {
+            item.push(replaceNewLineAndSpace($(this).text()));
+        });
+        items.push(item);
+    });
+    return items;
+}
--- test/server/parsers/ResumeParserSpec.js
+++ test/server/parsers/ResumeParserSpec.js
@@ -2,12 +2,20 @@ var fs = require('fs'),
     resumeParser = require('../../../server/parsers/resumeParser'),
     expect = require('chai').expect;
 
-describe.only('ResumeParser', function () {
+describe('ResumeParser', function () {
 
     describe('#parse', function () {
         it('should parse the resume correctly', function (done) {
             fs.readFile(__dirname + '/zhaopinresume.html', 'utf-8', function (err, data) {
                 var resume = resumeParser.parse(data);
+                console.log(resume);
+                done(err);
+            });
+        });
+
+        it.only('should parse 51job resume correctly', function (done) {
+            fs.readFile(__dirname + '/51jobresume.html', 'utf-8', function (err, data) {
+                var resume = resumeParser.parse51Job(data);
                 console.log(resume);
                 done(err);
             });
