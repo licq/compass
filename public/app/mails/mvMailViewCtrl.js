71189296d4e4fbfb4d5882a33e25d93111b52bbb
Li Chaoqun <lee.chaoqun@gmail.com>
2014-04-03 22:33:02 +0800

    add safe html to mvmailviewctrl

--- public/app/mails/mvMailViewCtrl.js
+++ public/app/mails/mvMailViewCtrl.js
@@ -1,6 +1,8 @@
 angular.module('compass')
-    .controller('mvMailViewCtrl', function ($scope, mvMail, $routeParams, $location) {
+    .controller('mvMailViewCtrl', function ($scope, mvMail, $routeParams, $location, $sce) {
         $scope.mail = mvMail.get({_id: $routeParams.id});
+
+        $scope.mail.html = $sce.trustAsHtml($scope.mail.html);
 
         $scope.close = function () {
             $location.path('/mails');
