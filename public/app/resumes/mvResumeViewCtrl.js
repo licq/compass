fe236f4100242baf721c91da20d1563b819e5cc7
Li Chaoqun <lee.chaoqun@gmail.com>
2014-04-08 20:34:12 +0800

    add original mail tab to resume view page

--- public/app/resumes/mvResumeViewCtrl.js
+++ public/app/resumes/mvResumeViewCtrl.js
@@ -1,4 +1,8 @@
 angular.module('compass')
     .controller('mvResumeViewCtrl', function ($scope, mvResume, $routeParams) {
         $scope.resume = mvResume.get({_id: $routeParams.id});
+
+        $scope.setMailHtml = function(){
+            $scope.mailHtml = $scope.mailHtml || '/api/mails/' + $scope.resume.mail + '/html';
+        }
     });
