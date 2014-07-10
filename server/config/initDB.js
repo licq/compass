38b1e2e86bc316cd67785aa362e63e673997c095
Yu Zhihang <zhihangyu@gmail.com>
2014-07-10 20:42:47 +0800

    add cancel button to interviews list and init db for applierRejectReason

---
+++ server/config/initDB.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  _ = require('lodash'),
+  User = mongoose.model('User'),
+  ApplierRejectReason = mongoose.model('ApplierRejectReason');
+
+exports.init = function () {
+  User.createSystemAdmin();
+  var reasons = ['薪资福利不满意', '有更好的发展机会','原公司挽留','工作地点离家远','想回老家发展','继续深造','无法按时入职','失联','其他'];
+
+  ApplierRejectReason.count(function(err,count){
+    if(!err && !count){
+      _.forEach(reasons, function (reason) {
+        ApplierRejectReason.create({reason: reason}, function(err){
+          if(err) return;
+        });
+      });
+    }
+  });
+
+
+};
--- server/config/systemAdmin.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose'),
-  User = mongoose.model('User');
-
-exports.init = function () {
-  User.createSystemAdmin();
-};
