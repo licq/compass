612a6f620f939db242c8689b5656a00ce8baee71
Yu Zhihang <zhihangyu@gmail.com>
2014-07-18 11:42:02 +0800

    user new view with checkbox

--- public/app/users/mvUserNewCtrl.js
+++ public/app/users/mvUserNewCtrl.js
@@ -1,13 +1,37 @@
 angular.module('compass')
-  .controller('mvUserNewCtrl', function ($scope, $location, mvUser, mvPosition, mvRole, mvNotifier) {
+  .controller('mvUserNewCtrl', function ($scope, mvApplicationSetting, $location, mvUser, mvPosition, mvRole, mvNotifier) {
     mvRole.query(function (roles) {
       $scope.roles = roles;
       mvPosition.query({fields: 'name'}, function (positions) {
         $scope.positions = positions;
         $scope.user = new mvUser();
+        $scope.selectAll = false;
+        angular.forEach($scope.positions, function (position) {
+          position.checked = false;
+        });
+        mvApplicationSetting.get({fields: 'positionRightControlled'}, function (settings) {
+          $scope.positionRightControlled = settings.positionRightControlled;
+          $scope.dataReady = true;
+        });
       });
     });
+
+    $scope.onSelectAll = function () {
+      angular.forEach($scope.positions, function (position) {
+        position.checked = $scope.selectAll;
+      });
+    };
+
+    $scope.onSelectPosition = function () {
+      $scope.selectAll = _.every($scope.positions, 'checked');
+    };
+
     $scope.create = function () {
+      $scope.user.positions = [];
+      angular.forEach($scope.positions, function (position) {
+        if (position.checked)
+          $scope.user.positions.push(position._id);
+      });
       $scope.user.$save(function () {
         $location.path('/settings/users');
         mvNotifier.notify('添加用户成功');
